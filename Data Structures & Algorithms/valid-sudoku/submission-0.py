5676c81711144524ba5504ee840b22d3f0561133
Noah Brom <87403027+noahsbrom@users.noreply.github.com>
2026-07-13 23:05:19 -0600

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        dr = defaultdict(set)
+        dc = defaultdict(set)
+        dq = defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == '.':
+                    continue
+                
+                q = (r//3 * 3) + c//3
+                if val in dr[r] or val in dc[c] or val in dq[q]:
+                    print(r, c, q, val)
+                    print(dr, dc, dq)
+                    return False
+                
+                dr[r].add(val)
+                dc[c].add(val)
+                dq[q].add(val)
+        return True
